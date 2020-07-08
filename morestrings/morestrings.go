c54ce03c645ab3cf1c56ba96e3f6c541d75ab9fb
basilkr <basil.krikeles@gmail.com>
2020-07-08 15:16:06 -0400

    Update morestrings.go

--- morestrings/morestrings.go
+++ morestrings/morestrings.go
@@ -3,7 +3,7 @@
 package morestrings
 
 // ReverseRunes returns its argument string reversed rune-wise left to right.
-func ReverseRunes(s string) string {
+func ReverseRunes(s string) string { // additional comment
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
